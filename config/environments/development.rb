357a2208bc8ee2fb8a6c5e5e8faad43242b388a4
Bruno Bergher <bruno@roocode.com>
2026-02-20 13:05:17 +0000

    Allow *.vercel.run hosts in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   config.hosts << "web-bruno.ngrok.dev"
   config.hosts << "preview-bruno.ngrok.app"
   config.hosts << "preview.roocode.cloud"
+  config.hosts << /.*\.vercel\.run\z/
 
   config.action_cable.url = ENV["ACTION_CABLE_URL"] if ENV["ACTION_CABLE_URL"].present?
   config.action_cable.allowed_request_origins = ENV["ALLOWED_ORIGINS"]&.split(",") if ENV["ALLOWED_ORIGINS"].present?
